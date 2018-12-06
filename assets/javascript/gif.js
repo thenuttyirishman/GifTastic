c614665583a2c3c10ad7143d6a8143a659cd350a
Matthew McGee <mrmatthewmcgee@gmail.com>
2018-12-06 18:32:33 -0500

    new

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,47 +1,73 @@
 // Setting API key as a variable
-var api_key='uviUHQfr8LQxpWLAQ8TU6rmPV1lY53Ra';
+var api_key = 'uviUHQfr8LQxpWLAQ8TU6rmPV1lY53Ra';
 // Declaring variables for each button
-var topics = ["Batman", "Superman", "Aquaman", "Green Lantern", "Green Arrow", "The Flash", "Wonder Woman", 
+var topics = ["Batman", "Superman", "Aquaman", "Green Lantern", "Green Arrow", "The Flash", "Wonder Woman",
     "Deadpool", "Spiderman", "Iron Man", "Thor", "Captain America"];
 // Variable for user input
-var input; 
+var input;
 // Initializing the document
-$( document ).ready(function() {
-// For loop to dynamicly convert our topics array into buttons
-    for(var i = 0; i < topics.length; i++) {
-    var buttons = $('<button>'+ topics[i]+'</button>')
-    buttons.appendTo('#topics');
-}
-// Telling the giphy API what keywords to search for
-var searchString = "superHeros";
-// Making the search form input user data
-$('#search').on('click', function(event){
-    event.preventDefault();
-    input = $('#input').val().trim();
-    topics.push(input);
-    topics.empty();
-})  
-
-function renderButtons() {
-    $("#search").empty();
-    for (var i = 0; i < topics.length; i++){
+$(document).ready(function () {
+    // For loop to dynamicly convert our topics array into buttons
+    for (var i = 0; i < topics.length; i++) {
+        var buttons = $('<button>' + topics[i] + '</button>')
+        buttons.appendTo('#topics');
 
     }
-}
+    // Telling the giphy API what keywords to search for
+    var searchString = "superHeros";
+    // Making the search form input user data
+    $('#search').on('click', function (event) {
+        event.preventDefault();
+        input = $('#input').val().trim();
+        topics.push(input);
+        renderButtons();
+        ajaxcall(input);
+    });
 
-//end point + 
-//operation    search?q= somestring
-//api_key
+    function renderButtons() {
+        $("#topics").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var buttons = $('<button>' + topics[i] + '</button>')
+            buttons.appendTo('#topics');
+        }
+    }
 
-// Contecting the giphy API and the API key
-var geturl = "https://api.giphy.com/v1/gifs/search?q="+searchString+"&api_key="+api_key;
-// Ajax "GET" method for giphys related to my search string
-$.ajax({
-    method: 'GET',
-    url: geturl,
-}) 
-.then(function(response) {
-    console.log(response);
-});
+    // Render buttons function
+    renderButtons();
+    //end point + 
+    //operation    search?q= somestring
+    //api_key
 
+    // Contecting the giphy API and the API key
+    function ajaxcall(searchString) {
+
+
+        var geturl = "https://api.giphy.com/v1/gifs/search?q=" + searchString + "&api_key=" + api_key;
+        // Ajax "GET" method for giphys related to my search string
+
+        $.ajax({
+            method: 'GET',
+            url: geturl,
+            success: function(response) {
+                console.log(response);
+                for(var i = 0; i < response.length; i++) {
+                    console.log("here");
+                    var gif = response.data[i].images.original;
+                    console.log("GIF: ", gif);
+                    var gifdiv = '<div><img src = ' + gif + '></div>';
+                    $('#giphys').append(gifdiv);
+                 }
+            }
+        })
+            // .then(function (response) {
+            //     console.log(response);
+            //     for(var i = 0; i < response.length; i++) {
+            //         console.log("here");
+            //         var gif = response.data[i].images.original;
+            //         console.log("GIF: ", gif);
+            //         var gifdiv = '<div><img src = ' + gif + '></div>';
+            //         $('#giphys').append(gifdiv);
+            //      }
+            // });
+    }
 });
