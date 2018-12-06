f10702b8da4f503c0fec78cd4ec631a6594478c7
Matthew McGee <mrmatthewmcgee@gmail.com>
2018-12-05 19:29:59 -0500

    new

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -2,7 +2,9 @@
 var api_key='uviUHQfr8LQxpWLAQ8TU6rmPV1lY53Ra';
 // Declaring variables for each button
 var topics = ["Batman", "Superman", "Aquaman", "Green Lantern", "Green Arrow", "The Flash", "Wonder Woman", 
-    "Deadpool", "Spiderman", "Iron Man", "Thor", "Captain America"]
+    "Deadpool", "Spiderman", "Iron Man", "Thor", "Captain America"];
+// Variable for user input
+var input; 
 // Initializing the document
 $( document ).ready(function() {
 // For loop to dynamicly convert our topics array into buttons
@@ -12,6 +14,20 @@ $( document ).ready(function() {
 }
 // Telling the giphy API what keywords to search for
 var searchString = "superHeros";
+// Making the search form input user data
+$('#search').on('click', function(event){
+    event.preventDefault();
+    input = $('#input').val().trim();
+    topics.push(input);
+    topics.empty();
+})  
+
+function renderButtons() {
+    $("#search").empty();
+    for (var i = 0; i < topics.length; i++){
+
+    }
+}
 
 //end point + 
 //operation    search?q= somestring
